ca6dec9aa9c65a4e45981e43a49156ecacdbdd24
Eddie Wen <eddiewen.tw@gmail.com>
2018-09-24 15:21:05 +0800

    test: add some basic feature tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,5 +1,35 @@
 const urlGlue = require('../lib');
 
-test('hello-jest', () => {
-  expect(1).toBeTruthy();
+test('connect each path parameters to become a url', () => {
+	expect(
+		urlGlue('https://www.example.com', 'foo'),
+	).toBe('https://www.example.com/foo/');
+});
+
+describe('connect paths with slashes', () => {
+	test('no path starts / ends with slashes', () => {
+		expect(
+			urlGlue('https://www.example.com', 'foo', 'bar'),
+		).toBe('https://www.example.com/foo/bar/');
+	});
+
+	test('each path starts / ends with slashes', () => {
+		expect(
+			urlGlue('https://www.example.com/', '/foo/', '/bar/'),
+		).toBe('https://www.example.com/foo/bar/');
+	});
+
+	describe('the last path', () => {
+		test('should end with a slash, if it\'s a folder', () => {
+			expect(
+				urlGlue('https://www.example.com', 'foo/'),
+			).toBe('https://www.example.com/foo/');
+		});
+
+		test('should not end with a slash, if it\'s a file', () => {
+			expect(
+				urlGlue('https://www.example.com', 'index.html'),
+			).toBe('https://www.example.com/index.html');
+		});
+	});
 });
